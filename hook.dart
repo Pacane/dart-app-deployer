03c1d2062682f67f83cc8b1d687930351e6f3528
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-11-18 18:55:41 -0500

    using streams

--- hook.dart
+++ hook.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
-import 'dart:convert' show JSON;
+import 'dart:convert' show JSON, UTF8, Encoding;
 import 'dart:convert' show UTF8;
 import 'package:crypto/crypto.dart' show SHA1, HMAC;
 
 void main() {
-    HttpServer.bind("stacktrace.ca" , 3001)
+    HttpServer.bind("localhost" , 3001)
         .then((HttpServer server) {
             print('listening on localhost, port ${server.port}');
             server.listen((HttpRequest request) {
                 String signature = request.headers.value("x-hub-signature");
                 var b = new HMAC(new SHA1(), UTF8.encode("lolol"));
-                UTF8.decodeStream(request).then((r) {
-                  b.add(UTF8.encode(r));
-                  var result = b.digest;
+                request.listen((data) {
+                  b.add(data);
+                  var result = UTF8.decode(b.digest);
                   print("sha1=$result");
                 });
 
