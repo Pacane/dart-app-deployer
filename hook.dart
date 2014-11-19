2a775cfbd778d56cab1b7c3bf3b82177f795b30c
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-11-18 19:19:46 -0500

    Compared with x-hub-sig

--- hook.dart
+++ hook.dart
@@ -1,38 +1,37 @@
 import 'dart:io';
-import 'dart:convert' show JSON, UTF8, Encoding;
 import 'dart:convert' show UTF8;
 import 'package:crypto/crypto.dart' show SHA1, HMAC, CryptoUtils;
 
 void main() {
-    HttpServer.bind("stacktrace.ca" , 3001)
-        .then((HttpServer server) {
-            print('listening on localhost, port ${server.port}');
-            server.listen((HttpRequest request) {
-                String signature = request.headers.value("x-hub-signature");
-                var b = new HMAC(new SHA1(), UTF8.encode("lolol"));
-                request.listen((data) {
-                  b.add(data);
-                  var digest = b.close();
+  var env = Platform.environment;
+  var token = env["GITHUB_TOKEN"];
 
-                  var hash = CryptoUtils.bytesToHex(digest);
-                  print("sha1=$hash");
-                });
+  if (token == null) {
+    throw new Exception("GITHUB_TOKEN is not set in environment variables.");
+  }
 
-                print(signature);
-//                var body = UTF8.decodeStream(request);
-//                body.then((data) {
-//                    var decoded = JSON.decode(data);
-//                    if (decoded["repository"]["id"] == 26665190) {
-//                        print("is a hook");
-//                        Process.start("./start-server.sh", []).then((Process process) {
-//                          process.stdout
-//                              .transform(UTF8.decoder)
-//                             .listen((data) => print(data));
-//                        });
-//                    }
-                    request.response.close();
-                });
-            });
-//        }).catchError((e) => print(e.toString()));
+  HttpServer.bind("stacktrace.ca", 3001)
+  .then((HttpServer server) {
+    print('listening on localhost, port ${server.port}');
+    server.listen((HttpRequest request) {
+      String signature = request.headers.value("x-hub-signature");
+      var sha = new HMAC(new SHA1(), UTF8.encode(token));
+      request.listen((data) {
+        sha.add(data);
+        var digest = sha.close();
+        var hash = CryptoUtils.bytesToHex(digest);
+
+        if (signature == hash) {
+          Process.start("./start-server.sh", []).then((Process process) {
+            process.stdout
+            .transform(UTF8.decoder)
+            .listen((data) => print(data));
+          });
+        }
+      });
+
+      request.response.close();
+    });
+  }).catchError((e) => print(e.toString()));
 }
 
