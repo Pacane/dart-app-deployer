76c4724436a09868b561a632becf4937c3292238
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-12-09 11:43:35 -0500

    Decommented github code

--- hook.dart
+++ hook.dart
@@ -9,23 +9,21 @@ void main() {
   var env = Platform.environment;
   var token = env["GITHUB_TOKEN"];
 
-//  if (token == null) {
-//    throw new Exception("GITHUB_TOKEN is not set in environment variables.");
-//  }
+  if (token == null) {
+    throw new Exception("GITHUB_TOKEN is not set in environment variables.");
+  }
 
   HttpServer.bind("localhost", 3002)
   .then((HttpServer server) {
     print('listening on localhost, port ${server.port}');
     server.listen((HttpRequest request) {
-//      String signature = request.headers.value("x-hub-signature");
-//      var sha = new HMAC(new SHA1(), UTF8.encode(token));
+      String signature = request.headers.value("x-hub-signature");
+      var sha = new HMAC(new SHA1(), UTF8.encode(token));
       request.listen((data) {
-//        sha.add(data);
-//        var digest = sha.close();
-//        var hash = CryptoUtils.bytesToHex(digest);
-//        if (signature == "sha1=$hash") {
-        if (1 == 1) {
-          print("if");
+        sha.add(data);
+        var digest = sha.close();
+        var hash = CryptoUtils.bytesToHex(digest);
+        if (signature == "sha1=$hash") {
           if (serverProcess != null) {
             print("killing server");
             serverProcess.kill();
@@ -35,6 +33,7 @@ void main() {
             print("killing client");
             clientProcess.kill();
           }
+
           print("Resetting branch");
           ProcessResult result = Process.runSync("bash", ["-c", "git pull && git reset --hard origin/master"], workingDirectory: "/home/joel/code/dart/PokerPlanning");
           print(result.stdout);
