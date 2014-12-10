8f7f40f2c44a5ee154b03b59b4cba750e1dcda81
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-12-10 15:21:06 -0500

    Process Run instead of Start

--- project_deployer.dart
+++ project_deployer.dart
@@ -3,20 +3,6 @@ import 'dart:convert' show UTF8;
 import 'dart:async';
 
 class ProjectDeployer {
-  Future deployNewSite() {
-    print("Deploying new site");
-    return Process.start("bash", ["-c", "cp $clientPath/build/web/* $websitePath -r"]).then((process) => showLogs(process));
-  }
-
-  Future removeOldWebsiteFiles() {
-    print("Removing old website files");
-    return Process.start("bash", ["-c", "rm -rf $websitePath/* -r"]).then((process) => showLogs(process));
-  }
-
-  deployClient() async {
-    buildWebsite().whenComplete(() => removeOldWebsiteFiles().whenComplete(() => deployClient()));
-  }
-
   Map config;
   var clientPath;
   var gitTarget;
@@ -47,9 +33,14 @@ class ProjectDeployer {
     process.stderr.transform(UTF8.decoder).listen((data) => print(data));
   }
 
+  void showLogsForProcessResult(ProcessResult processResult) {
+    print(processResult.stderr);
+    print(processResult.stdout);
+  }
+
   Future buildWebsite() {
     print("Building website");
-    return Process.start("bash", ["-c", "pub build"], workingDirectory : clientPath).then((process) => showLogs(process));
+    return Process.run("bash", ["-c", "pub build"], workingDirectory : clientPath).then((process) => showLogsForProcessResult(process));
   }
 
   void killServerProcess() {
@@ -67,5 +58,21 @@ class ProjectDeployer {
       serverProcess = process;
       showLogs(process);
     });
+  }
+
+  Future deployNewSite() {
+    print("Deploying new site");
+    return Process.run("bash", ["-c", "cp $clientPath/build/web/* $websitePath -r"]).then((process) => showLogsForProcessResult(process));
+  }
+
+  Future removeOldWebsiteFiles() {
+    print("Removing old website files");
+    return Process.run("bash", ["-c", "rm -rf $websitePath/* -r"]).then((process) => showLogsForProcessResult(process));
+  }
+
+  deployClient() async {
+    await buildWebsite();
+    await removeOldWebsiteFiles();
+    await deployNewSite();
   }
 }
